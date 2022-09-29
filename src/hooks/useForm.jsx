48d43c9566ea9889914054f51a00bc4f90db00f5
Alelsito <lbalelxander@gmail.com>
2022-09-29 15:25:17 -0500

    ✨ useForm customHook

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,28 @@
+// React
+import { useEffect, useState } from 'react'
+
+function useForm (callback, defaults) {
+  const [input, setInput] = useState(defaults)
+
+  useEffect(() => {
+    setInput({ ...defaults })
+  }, [])
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setInput({ ...input, [name]: value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    callback(input)
+  }
+
+  return {
+    input,
+    handleInputChange,
+    handleSubmit
+  }
+}
+
+export default useForm
