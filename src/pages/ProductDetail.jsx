22e5753e6243c7127fcb567fc7daa147c67e0658
Alelsito <lbalelxander@gmail.com>
2022-09-29 14:50:40 -0500

    ✨ Window scroll(0,0) for ProductDetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+// React
+import React, { useEffect } from 'react'
 
 // Components
 import ProductDescription from '../components/ProductDescription'
 import RelatedProducts from '../components/RelatedProducts'
 
 const ProductDetail = () => {
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
+
   return (
     <>
       <ProductDescription />
