1ccaa380fad7e7888c497604434394ee29830060
Alelsito <lbalelxander@gmail.com>
2022-10-01 18:06:18 -0500

    ✨ Creating page and route for Login and SignUp

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,12 +6,16 @@ import { Routes, Route } from 'react-router-dom'
 // Pages
 import Home from '../pages/Home'
 import ProductDetail from '../pages/ProductDetail'
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/product/:idProduct' element={<ProductDetail />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   )
 }
