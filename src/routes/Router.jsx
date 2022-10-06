81e765ac52294c482b67478f6a9d98fe76677040
Alelsito <lbalelxander@gmail.com>
2022-10-05 20:37:46 -0500

    🛂 Conditional routes (if isAuth is true and if the user has a CUSTOMER or an ADMIN role)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 // Router
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+// Context
+import { useAuthContext } from '../context/AuthContext'
 
 // Pages
 import Home from '../pages/Home'
@@ -11,13 +14,26 @@ import SignUp from '../pages/SignUp'
 import RegisterProduct from '../pages/RegisterProduct'
 
 const Router = () => {
+  const context = useAuthContext()
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/product/:idProduct' element={<ProductDetail />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/register/product' element={<RegisterProduct />} />
+      <Route
+        path='/login'
+        element={context.isAuth ? <Navigate to='/' replace /> : <Login />}
+      />
+      <Route
+        path='/signup'
+        element={context.isAuth ? <Navigate to='/' replace /> : <SignUp />}
+      />
+      <Route
+        path='/register/product'
+        element={context.isAuth && context.userInfo?.role === 'CUSTOMER'
+          ? <Navigate to='/' replace />
+          : context.userInfo?.role === 'ADMIN' &&
+            <RegisterProduct />}
+      />
     </Routes>
   )
 }
